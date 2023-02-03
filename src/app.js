c22727e9c51b1f2c848b09f41bc14b6274fbf7f3
guilhermeOsilva <guilhermeSilvaDev@outlook.com>
2023-02-03 06:17:09 -0300

    adicionando a conexão com mongodb

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+let mongoose = require("mongoose");
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1/picsgui", {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+ 
+}).catch(err => {
+
+})
+
 
 app.get("/", (req, res) => {
   res.json({ message: "success" });
